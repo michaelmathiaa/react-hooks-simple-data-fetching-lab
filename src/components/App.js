b708e43d32827e8b3936aa33b96e7b271d90fcf1
Michael Mathias <michaelscodingjourney@gmail.com>
2023-06-08 22:59:21 -0700

    Passed two tests for react hooks simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [randomDog, setRandomDog] = useState(false);
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            setRandomDog(data.message);
+        });
+    }, []);
+    return (
+        <div>
+            {randomDog ? <img src={randomDog} alt="A Random Dog" /> : <p>Loading...</p>}
+        </div>
+    )
+}
+
+export default App;
